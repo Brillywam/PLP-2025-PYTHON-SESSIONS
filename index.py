b4305f32cb331c232093317312d38e90a3811ee2
Brillywam <mweshwam5@gmail.com>
2025-03-11 11:50:37 +0300

    Initial commit

---
+++ index.py
@@ -0,0 +1,15 @@
+print("Hello world, again!")
+
+number = 10
+print(number)
+
+age = 45 #integer data type
+print (age)
+marks = 90.4 #float data type
+print (marks)
+name = "The name is BrillyWam" #string data type
+print(name)
+#lists data types
+languages = ["python", "java", "html", "CSS", 3, 5.66, True]
+print(languages)
+print(languages[-4])
